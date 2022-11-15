a1465f9f26f0bc7c7eed85929d1ea85cfcb698e1
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-11-15 15:01:28 +0900

    feat: image_picker

--- lib/pages/settings_profile.dart
+++ lib/pages/settings_profile.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SettingsProfilePage extends StatefulWidget {
   const SettingsProfilePage({Key? key}) : super(key: key);
@@ -8,6 +10,18 @@ class SettingsProfilePage extends StatefulWidget {
 }
 
 class _SettingsProfilePageState extends State<SettingsProfilePage> {
+  File? image;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> selectImage() async {
+    PickedFile? pickedImage = await _picker.getImage(source: ImageSource.gallery);
+    if (pickedImage == null) return;
+
+    setState(() {
+      image = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,23 +39,41 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
               ],
             ),
             SizedBox(
-              height: 30,
+              height: 50,
             ),
             Row(
               children: [
-                Container(width: 100.0, child: Text("サムネイル")),
+                const SizedBox(width: 150.0, child: Text("プロフィール画像")),
                 Expanded(
                   child: Container(
                     alignment: Alignment.center,
                     child: Container(
                         width: 150,
                         height: 40,
-                        child:
-                            ElevatedButton(onPressed: () {}, child: Text("画像選択"))),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              selectImage();
+                            },
+                            child: Text("画像選択"))),
                   ),
                 )
               ],
-            )
+            ),
+            const SizedBox(height: 30.0),
+            image == null
+                ? SizedBox(
+                    height: 150.0,
+                  )
+                : SizedBox(
+                    width: 200, height: 200, child: Image.file(image!, fit: BoxFit.cover,)),
+            const SizedBox(height: 30.0),
+            SizedBox(
+              width: 150.0,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("編集"),
+              ),
+            ),
           ],
         ),
       ),
